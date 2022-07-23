0a4caa855024a19a715c313037c8ca2e2dbec2e1
Francesco La Rosa <85963091+FrankLaro@users.noreply.github.com>
2022-07-23 18:41:38 +0200

    Update brainfuck_interpreter.c

--- brainfuck_interpreter.c
+++ brainfuck_interpreter.c
@@ -2,11 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
-# define ARR_DIMENSION 30000 
+#define ARR_DIMENSION 30000 
 char array[ARR_DIMENSION];
 int cursor = 0;
 int start_loop = -1;
 int end_loop   = -1;
+
+// test function
+void test(char arr[], int arr_size){
+    int i;
+    for (i = 0; i < arr_size; i++){
+        printf("%c", arr[i]);
+    }
+    printf("\n");
+    sleep(1);
+}
 
 // * WORKS FINE
 // determinate how long the file is
@@ -41,14 +51,24 @@ void copyCodeFromFile(char file_name[], char arr[], int arr_size) {
     fclose(f);
 }
 
+// move array's cursor
 void moveCursor(char command) {
-    if (command == '>') cursor++; 
-    if (command == '<') cursor--;
+    if (command == '>') { 
+        cursor++;
+    } 
+    if (command == '<') {
+        cursor--;
+    }
     
-    if (cursor > 29999) cursor = 0;
-    if (cursor < 0)     cursor = 29999;
+    if (cursor > 29999) {
+        cursor = 0;
+    }
+    if (cursor < 0) {
+        cursor = 29999;
+    }
 }
 
+// copy loop code
 char* loop(char code[], int start, int end) {
     int i;
     int loop_lenght = end - start - 1;
@@ -60,67 +80,62 @@ char* loop(char code[], int start, int end) {
 
     return temp_code;
 }
-/*
- BrainFuck commands
 
- > -> move forward
- < -> move backward
- + -> add 1
- - -> sub 1
- . -> output
- , -> input
- [ -> open loop
- ] -> close loop
-
-*/
-void interpreter(char code[], int dimension) {                  
+// * BrainFuck commands
+// *
+// * > -> move forward
+// * < -> move backward
+// * + -> add 1
+// * - -> sub 1
+// * . -> output
+// * , -> input
+// * [ -> open loop
+// * ] -> close loop
+void interpreter(char code[], int dimension) {
     int i, j;                                                      
     int bracket_counter = 0;                                                      
-    for (i = 0; i < dimension; i++) {                            
-        if (code[i] == '>' || code[i] == '<') {
+    for (i = 0; i < dimension; i++) {                                              
+        if (code[i] == '>' || code[i] == '<') { // * move cursor
             moveCursor(code[i]);
-        } else if (code[i] == '+') {
+        } else if (code[i] == '+') {            // * increment value
             array[cursor]++;
-        } else if (code[i] == '-') {
+        } else if (code[i] == '-') {            // * decrese value
             array[cursor]--;
-        } else if (code[i] == '.') {
+        } else if (code[i] == '.') {            // * output
             printf("%c", array[cursor]);
-        } else if (code[i] == ',') {
-            scanf("%c", &array[cursor]);
-        } else if (code[i] == '[') {
+        } else if (code[i] == ',') {            // * input
+            scanf(" %c", &array[cursor]);
+        // * start loop code
+        } else if (code[i] == '[') {            
             start_loop = i;
-            for (j = i; j < dimension; j++) {
+            for (j = start_loop + 1; j < dimension; j++) {
                 if (code[j] == '[') {
                     bracket_counter++;
                 } else if (code[j] == ']') {
-                    if (bracket_counter != 0) bracket_counter--;
-                        else {
+                    if (bracket_counter != 0) {
+                        bracket_counter--;
+                    } else {
                             end_loop = j;
                             break;
                         }
                 }
             }
-            while(array[cursor] != 0) interpreter(loop(code, start_loop, end_loop), end_loop - start_loop + 1);
+            i = end_loop + 1;
+            while(array[cursor] != 0) {
+                interpreter(loop(code, start_loop, end_loop), end_loop - start_loop + 1);
+            }
         }
-    }
-}
-
-// test function
-void test(char arr[], int arr_size){
-    int i;
-    for (i = 0; i < arr_size; i++){
-        if (arr[i] == '\n') printf(" OK BRO");
-        printf("%c", arr[i]);
+        // * end loop code
     }
 }
 
 int main(int argc, char *file_path[]) {
-    int code_dimension = numberOfCommand(file_path[1]) * sizeof(char); // dimension of array where code is stored                                        // dimension of BrainFuck array
+    int code_dimension = numberOfCommand(file_path[1]) * sizeof(char); // dimension of array where code is stored
     char* code  = malloc(code_dimension);
 
     copyCodeFromFile(file_path[1], code, code_dimension);
+    // ? test(code, code_dimension);
     interpreter(code, code_dimension);
-    test(code, code_dimension);
     free(code);
     return 0;
 }
